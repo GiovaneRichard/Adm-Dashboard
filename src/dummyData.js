d23acd9b9568c279858a5adac97332e434fb3051
Giovane Richard <giovanerichard@gmail.com>
2021-12-30 17:21:10 -0300

    Create Chart

---
+++ src/dummyData.js
@@ -0,0 +1,51 @@
+export const UserData = [
+    {
+      name: 'Jan',
+      Usuarios_ativos: 4000,
+    },
+    {
+        name: 'Fev',
+        Usuarios_ativos: 2545,
+    },
+    {
+        name: 'Mar',
+        Usuarios_ativos: 3657,
+    },
+    {
+        name: 'Abr',
+        Usuarios_ativos: 1231,
+    },
+    {
+        name: 'Mai',
+        Usuarios_ativos: 7852,
+    },
+    {
+        name: 'Jun',
+        Usuarios_ativos: 1235,
+    },
+    {
+        name: 'Jul',
+        Usuarios_ativos: 9012,
+    },
+    {
+        name: 'Ago',
+        Usuarios_ativos: 5021,
+    },
+    {
+        name: 'Set',
+        Usuarios_ativos: 4596,
+    },
+    {
+        name: 'Out',
+        Usuarios_ativos: 3564,
+    },
+    {
+        name: 'Nov',
+        Usuarios_ativos: 4256,
+    },
+    {
+        name: 'Dez',
+        Usuarios_ativos: 9564,
+    },
+    
+];
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import './Home.css';
 import FeaturedInfo from "../../components/featuredInfo/FeaturedInfo";
-
+import Chart from "../../components/chart/Chart";
+import { UserData } from "../../dummyData";
 
 export default () => {
     return(
         <div className="home">
             <FeaturedInfo />
+            <Chart data={UserData} title="Análise de Usuários" grid dataKey="Usuarios_ativos" />
+            <div className="home--widgets"></div>
         </div>
     );
 }
